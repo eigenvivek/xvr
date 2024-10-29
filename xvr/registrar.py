472206550963b9bc70d7d16b04a6f86ec5a0f2be
eigenvivek <vivekg@mit.edu>
2024-10-29 16:57:25 -0400

    Implement inheritence for multiple init strategies

--- xvr/registrar.py
+++ xvr/registrar.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -10,49 +11,43 @@ from diffdrr.metrics import (
 from diffdrr.registration import Registration
 from diffdrr.visualization import plot_drr
 
-from .dicom import _parse_dicom, _preprocess_xray
-from .renderer import initialize_drr
-from .model import load_model, predict_pose, _correct_pose
-from .utils import XrayTransforms
+from xvr.dicom import read_xray, _parse_dicom_pose
+from xvr.renderer import initialize_drr
+from xvr.model import load_model, predict_pose, _correct_pose
+from xvr.utils import XrayTransforms
 
 
-class Registrar:
+class RegistrarBase:
     def __init__(
         self,
         volume,
         mask,
-        ckptpath,
-        crop=0,
-        subtract_background=False,
-        linearize=True,
-        warp=None,
-        invert=False,
-        model_only=False,
-        labels=None,
-        scales="8",
-        reverse_x_axis=True,
-        renderer="trilinear",
-        parameterization="euler_angles",
-        convention="ZXY",
-        lr_rot=1e-2,
-        lr_xyz=1e0,
-        patience=10,
-        threshold=1e-4,
-        max_n_itrs=500,
-        max_n_plateaus=3,
-        verbose=2,
-        read_kwargs={},
-        drr_kwargs={},
+        orientation,
+        labels,
+        crop,
+        subtract_background,
+        linearize,
+        scales,
+        reverse_x_axis,
+        renderer,
+        parameterization,
+        convention,
+        lr_rot,
+        lr_xyz,
+        patience,
+        threshold,
+        max_n_itrs,
+        max_n_plateaus,
+        init_only,
+        verbose,
+        read_kwargs,
+        drr_kwargs,
     ):
-        # Initialize the model and its config
-        self.ckptpath = ckptpath
-        self.model, self.config, self.date = load_model(self.ckptpath, meta=True)
-        self.model_only = model_only
-
         # Initialize a DRR object with placeholder intrinsic parameters
         # These are reset after a real DICOM file is parsed
         self.volume = volume
         self.mask = mask
+        self.orientation = orientation
         self.labels = labels
         self.reverse_x_axis = reverse_x_axis
         self.renderer = renderer
@@ -61,7 +56,7 @@ class Registrar:
             self.volume,
             self.mask,
             self.labels,
-            self.config["orientation"],
+            self.orientation,
             height=1436,
             width=1436,
             sdd=1020.0,
@@ -87,10 +82,6 @@ class Registrar:
         self.subtract_background = subtract_background
         self.linearize = linearize
 
-        # Initial pose correction
-        self.warp = warp
-        self.invert = invert
-
         # Registration SE(3) parameterization
         self.parameterization = parameterization
         self.convention = convention
@@ -103,21 +94,18 @@ class Registrar:
         self.threshold = threshold
         self.max_n_itrs = max_n_itrs
         self.max_n_plateaus = max_n_plateaus
+
+        # Misc parameters
+        self.init_only = init_only
         self.verbose = verbose
+
+    def initialize_pose(self, i2d):
+        """Get initial pose estimate and image intrinsics."""
+        raise NotImplementedError
 
     def run(self, i2d):
         # Predict the initial pose with a pretrained network
-        gt, sdd, delx, dely, x0, y0, init_pose, self.height = initialize_pose(
-            i2d,
-            self.model,
-            self.config,
-            self.crop,
-            self.subtract_background,
-            self.linearize,
-            self.warp,
-            self.volume,
-            self.invert,
-        )
+        gt, sdd, delx, dely, x0, y0, init_pose = self.initialize_pose(i2d)
         *_, height, width = gt.shape
         intrinsics = {
             "sdd": sdd,
@@ -131,15 +119,15 @@ class Registrar:
 
         # Update the DRR's intrinsic parameters
         self.drr.set_intrinsics_(**intrinsics)
-        if self.model_only:
-            return gt, intrinsics, self.drr, init_pose, None, {}
+        if self.init_only:
+            return gt, intrinsics, deepcopy(self.drr), init_pose, None, {}
 
         # Initialize the diffdrr.registration.Registration module
         rot, xyz = init_pose.convert(self.parameterization, self.convention)
         reg = Registration(self.drr, rot, xyz, self.parameterization, self.convention)
 
         # Parse the scales for multiscale registration
-        scales = _parse_scales(self.scales, self.crop, self.height)
+        scales = _parse_scales(self.scales, self.crop, height)
 
         # Perform multiscale registration
         params = [
@@ -192,7 +180,7 @@ class Registrar:
                 scheduler.step(loss)
 
                 # Record current loss
-                if self.verbose > 1:
+                if self.verbose > 0:
                     pbar.set_postfix_str(f"ncc = {loss.item():5.3f}")
                 nccs.append(loss.item())
                 params.append(
@@ -233,7 +221,7 @@ class Registrar:
         return (
             gt,
             intrinsics,
-            self.drr,
+            deepcopy(self.drr),
             init_pose,
             reg.pose,
             dict(trajectory=trajectory),
@@ -247,9 +235,90 @@ class Registrar:
         init_pose = init_pose.matrix.detach().cpu()
         if final_pose is not None:
             final_pose = final_pose.matrix.detach().cpu()
+        self.save(savepath, gt, intrinsics, init_pose, final_pose, kwargs)
+
+    def save(self, savepath, i2d, gt, intrinsics, init_pose, final_pose, kwargs):
+        raise NotImplementedError
+
+
+class RegistrarModel(RegistrarBase):
+    def __init__(
+        self,
+        volume,
+        mask,
+        ckptpath,
+        labels=None,
+        crop=0,
+        subtract_background=False,
+        linearize=True,
+        warp=None,
+        invert=False,
+        scales="8",
+        reverse_x_axis=True,
+        renderer="trilinear",
+        parameterization="euler_angles",
+        convention="ZXY",
+        lr_rot=1e-2,
+        lr_xyz=1e0,
+        patience=10,
+        threshold=1e-4,
+        max_n_itrs=500,
+        max_n_plateaus=3,
+        init_only=False,
+        verbose=2,
+        read_kwargs={},
+        drr_kwargs={},
+    ):
+        # Initialize the model and its config
+        self.ckptpath = ckptpath
+        self.model, self.config, self.date = load_model(self.ckptpath, meta=True)
+
+        # Initial pose correction
+        self.warp = warp
+        self.invert = invert
+
+        super().__init__(
+            volume,
+            mask,
+            self.config["orientation"],
+            labels,
+            crop,
+            subtract_background,
+            linearize,
+            scales,
+            reverse_x_axis,
+            renderer,
+            parameterization,
+            convention,
+            lr_rot,
+            lr_xyz,
+            patience,
+            threshold,
+            max_n_itrs,
+            max_n_plateaus,
+            init_only,
+            verbose,
+            read_kwargs,
+            drr_kwargs,
+        )
+
+    def initialize_pose(self, i2d):
+        # Preprocess X-ray image and get imaging system intrinsics
+        gt, sdd, delx, dely, x0, y0 = read_xray(
+            i2d, self.crop, self.subtract_background, self.linearize
+        )
+
+        # Predict the pose of the X-ray image
+        init_pose = predict_pose(self.model, self.config, gt, sdd, delx, dely, x0, y0)
+
+        # Optionally, correct the pose by warping the CT volume to the template
+        init_pose = _correct_pose(init_pose, self.warp, self.volume, self.invert)
+
+        return gt, sdd, delx, dely, x0, y0, init_pose
+
+    def save(self, savepath, i2d, gt, intrinsics, init_pose, final_pose, kwargs):
         mask = Path(self.mask).resolve() if self.mask is not None else None
         warp = Path(self.warp).resolve() if self.warp is not None else None
-
         torch.save(
             {
                 "arguments": {
@@ -257,13 +326,12 @@ class Registrar:
                     "volume": Path(self.volume).resolve(),
                     "mask": mask,
                     "ckptpath": Path(self.ckptpath).resolve(),
-                    "outpath": Path(outpath).resolve(),
                     "crop": self.crop,
                     "subtract_background": self.subtract_background,
                     "linearize": self.linearize,
                     "warp": warp,
                     "invert": self.invert,
-                    "model_only": self.model_only,
+                    "init_only": self.init_only,
                     "labels": self.labels,
                     "scales": self.scales,
                     "reverse_x_axis": self.reverse_x_axis,
@@ -288,35 +356,104 @@ class Registrar:
         )
 
 
-def initialize_pose(
-    i2d,
-    model,
-    config,
-    crop,
-    subtract_background,
-    linearize,
-    warp,
-    volume,
-    invert,
-    meta=True,
-):
-    """Get initial pose estimate and image intrinsics."""
+class RegistrarDicom(RegistrarBase):
+    def __init__(
+        self,
+        volume,
+        mask,
+        orientation,
+        labels=None,
+        crop=0,
+        subtract_background=False,
+        linearize=True,
+        scales="8",
+        reverse_x_axis=True,
+        renderer="trilinear",
+        parameterization="euler_angles",
+        convention="ZXY",
+        lr_rot=1e-2,
+        lr_xyz=1e0,
+        patience=10,
+        threshold=1e-4,
+        max_n_itrs=500,
+        max_n_plateaus=3,
+        init_only=False,
+        verbose=2,
+        read_kwargs={},
+        drr_kwargs={},
+    ):
+        super().__init__(
+            volume,
+            mask,
+            orientation,
+            labels,
+            crop,
+            subtract_background,
+            linearize,
+            scales,
+            reverse_x_axis,
+            renderer,
+            parameterization,
+            convention,
+            lr_rot,
+            lr_xyz,
+            patience,
+            threshold,
+            max_n_itrs,
+            max_n_plateaus,
+            init_only,
+            verbose,
+            read_kwargs,
+            drr_kwargs,
+        )
 
-    # Preprocess X-ray image and get imaging system intrinsics
-    gt, sdd, delx, dely, x0, y0 = _parse_dicom(i2d)
-    gt = _preprocess_xray(gt, crop, subtract_background, linearize)
+    def initialize_pose(self, i2d):
+        # Preprocess X-ray image and get imaging system intrinsics
+        gt, sdd, delx, dely, x0, y0 = read_xray(
+            i2d, self.crop, self.subtract_background, self.linearize
+        )
 
-    # Predict the pose of the X-ray image
-    init_pose, height = predict_pose(model, config, gt, sdd, delx, dely, x0, y0, meta)
+        # Parse the pose from dicom parameters
+        init_pose = _parse_dicom_pose(i2d, self.orientation).cuda()
 
-    # Optionally, correct the pose by warping the CT volume to the template
-    init_pose = _correct_pose(init_pose, warp, volume, invert)
+        return gt, sdd, delx, dely, x0, y0, init_pose
 
-    return gt, sdd, delx, dely, x0, y0, init_pose, height
+    def save(self, savepath, i2d, gt, intrinsics, init_pose, final_pose, kwargs):
+        mask = Path(self.mask).resolve() if self.mask is not None else None
+        torch.save(
+            {
+                "arguments": {
+                    "i2d": Path(i2d).resolve(),
+                    "volume": Path(self.volume).resolve(),
+                    "mask": mask,
+                    "crop": self.crop,
+                    "subtract_background": self.subtract_background,
+                    "linearize": self.linearize,
+                    "init_only": self.init_only,
+                    "labels": self.labels,
+                    "scales": self.scales,
+                    "reverse_x_axis": self.reverse_x_axis,
+                    "renderer": self.renderer,
+                    "parameterization": self.parameterization,
+                    "convention": self.convention,
+                    "lr_rot": self.lr_rot,
+                    "lr_xyz": self.lr_xyz,
+                    "patience": self.patience,
+                    "max_n_itrs": self.max_n_itrs,
+                    "max_n_plateaus": self.max_n_plateaus,
+                },
+                "gt": gt,
+                "intrinsics": intrinsics,
+                "init_pose": init_pose,
+                "final_pose": final_pose,
+                **kwargs,
+            },
+            savepath,
+        )
 
 
 def _parse_scales(scales: str, crop: int, height: int):
-    pyramid = [1.0] + [float(x) * (height / (height + crop)) for x in scales.split(",")]
+    pyramid = [1.0] + [float(x) * ((height - crop) / height) for x in scales.split(",")]
     scales = []
     for idx in range(len(pyramid) - 1):
         scales.append(pyramid[idx] / pyramid[idx + 1])
