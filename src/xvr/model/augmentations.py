4ee99d0837acf258074e0ce03896ef0fa4e70dcf
eigenvivek <vivekg@mit.edu>
2025-11-03 18:31:56 -0500

    Simplify augmentations

--- src/xvr/model/augmentations.py
+++ src/xvr/model/augmentations.py
@@ -8,18 +8,13 @@ from ..utils import Standardize
 
 
 def XrayAugmentations(
-    p=0.5, max_crop=10, same_on_batch=False, transformation_matrix_mode="skip"
+    p=0.333, max_crop=10, same_on_batch=False, transformation_matrix_mode="skip"
 ):
     return K.AugmentationSequential(
         Standardize(),
-        K.RandomEqualize(p=p / 3),
-        K.RandomBrightness(brightness=0.2, p=p),
-        K.RandomContrast(contrast=(0.8, 1.2), p=p),
-        K.RandomGamma(gamma=(0.7, 1.3), p=p),
+        K.RandomGamma(gamma=(0.7, 1.8), p=p),
         K.RandomBoxBlur(p=p),
         K.RandomGaussianNoise(std=0.01, p=p),
-        K.RandomInvert(p=p / 5),
-        K.RandomAutoContrast(p=p / 2),
         K.RandomSharpness(p=p),
         K.RandomErasing(p=p),
         RandomCenterCrop(p=p, maxcrop=max_crop),
