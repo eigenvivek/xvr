ece4fd95758c1ae5f44e6a622032b8904e674832
Vivek Gopalakrishnan <vivekg@mit.edu>
2025-11-23 13:28:42 -0500

    Refactor CLI (#97)
    * Add -h/-v shortcuts to groups
    
    * Refactor animation command
    
    * Remove deprecated finetune command
    
    * Update docstring

--- src/xvr/cli/cli.py
+++ src/xvr/cli/cli.py
@@ -5,7 +5,6 @@ import click
 
 from .commands.animate import animate
 from .commands.dcm2nii import dcm2nii
-from .commands.finetune import finetune
 from .commands.register import dicom, fixed, model
 from .commands.restart import restart
 from .commands.train import train
@@ -14,6 +13,10 @@ from .commands.train import train
 # Taken from https://stackoverflow.com/a/58323807
 class OrderedGroup(click.Group):
     def __init__(self, name=None, commands=None, **attrs):
+        attrs["context_settings"] = {
+            # "max_content_width": 120,
+            "help_option_names": ["-h", "--help"],
+        }
         super().__init__(name, commands, **attrs)
         self.commands = commands or OrderedDict()
 
@@ -36,17 +39,19 @@ register.add_command(fixed)
 
 
 @click.group(cls=OrderedGroup)
-@click.version_option(version("xvr"))
+@click.version_option(version("xvr"), "--version", "-v")
 @click.pass_context
 def cli(ctx):
     """
-    xvr is a PyTorch package for training, fine-tuning, and performing 2D/3D X-ray to CT/MR registration with pose regression models.
+    A PyTorch package for 2D/3D XRAY to CT/MR registration.
+
+    Provides functionality for rapidly training pose regression models and
+    registering clinical data with gradient-based iterative optimization.
     """
 
 
 cli.add_command(train)
 cli.add_command(restart)
-cli.add_command(finetune)
 cli.add_command(register)
 cli.add_command(animate)
 cli.add_command(dcm2nii)
--- src/xvr/cli/commands/animate.py
+++ src/xvr/cli/commands/animate.py
@@ -9,7 +9,7 @@ from ..formatter import CategorizedCommand, categorized_option
     "--inpath",
     required=True,
     type=click.Path(exists=True),
-    help="Saved registration result from `xvr register`",
+    help="Saved registration result from <xvr register>",
 )
 @categorized_option(
     "-o",
@@ -39,106 +39,6 @@ from ..formatter import CategorizedCommand, categorized_option
 def animate(inpath, outpath, skip, dpi, fps):
     """Animate the trajectory of iterative optimization."""
 
-    import torch
-    from imageio.v3 import imwrite
+    from ...visualization import animate as _animate
 
-    from ...io import read_xray
-    from ...registrar import _parse_scales
-    from ...renderer import initialize_drr
-
-    # Initialize the renderer
-    run = torch.load(inpath, weights_only=False)
-    drr = initialize_drr(**run["drr"])
-    gt, *_ = read_xray(**run["xray"])
-    scales = _parse_scales(
-        run["optimization"]["scales"], run["xray"]["crop"], run["drr"]["height"]
-    )
-
-    # Render all DRRs
-    drrs = render(drr, gt, scales, run, skip)
-
-    # Generate the animation
-    frames = plot(drrs, dpi)
-    imwrite(outpath, frames, fps=fps)
-
-
-def render(drr, gt, scales, run, skip):
-    import torch
-    from diffdrr.pose import convert
-    from tqdm import tqdm
-
-    from ...utils import XrayTransforms
-
-    lowest_lr = 0.0
-
-    drrs = []
-    for idx, row in tqdm(
-        run["trajectory"].iterrows(),
-        total=len(run["trajectory"]),
-        desc="Rendering DRRs",
-    ):
-        # Animate every <skip> frames
-        if idx % skip != 0:
-            continue
-
-        # If the learning rate has reset, rescale the detector
-        if row.lr_rot > lowest_lr:
-            scale = scales.pop(0)
-            drr.rescale_detector_(scale)
-            transform = XrayTransforms(drr.detector.height, drr.detector.width)
-            true = transform(gt)
-        lowest_lr = row.lr_rot
-
-        # Render the current estimate
-        pose = convert(
-            torch.tensor([[row.r1, row.r2, row.r3]]),
-            torch.tensor([[row.tx, row.ty, row.tz]]),
-            parameterization=run["optimization"]["parameterization"],
-            convention=run["optimization"]["convention"],
-        ).to(dtype=torch.float32, device="cuda")
-        with torch.no_grad():
-            pred = drr(pose)
-            pred = transform(pred)
-
-        # Save the results
-        drrs.append([true.cpu(), pred.cpu()])
-
-    return drrs
-
-
-def plot(drrs, dpi):
-    from pathlib import Path
-    from tempfile import TemporaryDirectory
-
-    import matplotlib.pyplot as plt
-    import torch
-    from diffdrr.visualization import plot_drr
-    from imageio.v3 import imread
-    from tqdm import tqdm
-
-    # Plot every frame of the animation
-    with TemporaryDirectory() as tmpdir:
-        for idx, img in tqdm(enumerate(drrs), total=len(drrs), desc="Rendering frames"):
-            imgs = [img[0], img[1], img[0] - img[1]]
-
-            plt.figure(figsize=(9, 3), dpi=dpi)
-            ax1 = plt.subplot(131)
-            ax2 = plt.subplot(132)
-            ax3 = plt.subplot(133)
-            plot_drr(
-                torch.concat(imgs),
-                axs=[ax1, ax2, ax3],
-                title=["Ground Truth", "Prediction", "Difference"],
-                # ticks=False,
-            )
-            plt.gcf().set_size_inches(9, 3)
-            plt.savefig(f"{tmpdir}/{idx:04d}.png", pad_inches=0, dpi=dpi)
-            plt.close()
-
-        # Read the images
-        imgs = []
-        for filepath in sorted(Path(tmpdir).glob("*.png")):
-            img = imread(filepath)
-            imgs.append(img)
-
-    return imgs
+    _animate(inpath, outpath, skip, dpi, fps)
