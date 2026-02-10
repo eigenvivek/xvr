bcd85d2f4719f1a4d2c8b17dab27251c298f1315
eigenvivek <vivekg@mit.edu>
2026-02-10 09:52:30 -0500

    Make XrayTransforms compilable

--- src/xvr/model/utils.py
+++ src/xvr/model/utils.py
@@ -158,7 +158,7 @@ def initialize_modules(
         dtype=torch.float32,
         device="cuda",
     )
-    transforms = XrayTransforms(height)
+    transforms = XrayTransforms(height).cuda()
 
     return model, drr, transforms, optimizer, scheduler, start_itr, model_number
 
