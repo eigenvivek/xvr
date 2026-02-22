83f8d993d5eda58f3c4718035713f8b174bc3b77
eigenvivek <vivekg@mit.edu>
2026-02-22 07:52:00 -0500

    Revert changes to xframe

--- src/xvr/model/initialize/xframe.py
+++ src/xvr/model/initialize/xframe.py
@@ -1,18 +1,7 @@
-from pathlib import Path
-
-import torch
-from torchio import ScalarImage
-
 from ...utils import get_4x4
 
 
 def initialize_coordinate_frame(warp, img, invert):
-    if warp is not None:
-        return get_4x4(warp, img, invert).cuda().matrix
-    if Path(img).is_file():
-        isocenter = ScalarImage(img).get_center()
-        isocenter = torch.tensor(isocenter).to(torch.float32)
-        transform = torch.eye(4, dtype=torch.float32)[None]
-        transform[:, :3, 3] = isocenter
-        return transform.cuda()
-    return None
+    if warp is None:
+        return None
+    return get_4x4(warp, img, invert).cuda().matrix
