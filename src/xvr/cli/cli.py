d4fac400feb92d575d08245f470628bd04ac9666
Vivek Gopalakrishnan <vivekg@mit.edu>
2026-03-02 09:03:00 -0500

    Deprecate dcm2nii

--- src/xvr/cli/cli.py
+++ src/xvr/cli/cli.py
@@ -3,7 +3,6 @@ from importlib.metadata import version
 
 import click
 
-from .commands.dcm2nii import dcm2nii
 from .commands.register import dicom, fixed, model
 from .commands.register import restart as restart_optimization
 from .commands.restart import restart as restart_training
@@ -54,4 +53,3 @@ def cli(ctx):
 cli.add_command(train)
 cli.add_command(restart_training)
 cli.add_command(register)
-cli.add_command(dcm2nii)
