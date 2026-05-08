b376989a4b38dc156af41554ddf3d8d61d6bebb6
eigenvivek <vivekg@mit.edu>
2026-05-08 16:44:47 -0400

    Add docstrings

--- src/xvr/model/trainer.py
+++ src/xvr/model/trainer.py
@@ -9,7 +9,6 @@ from diffdrr.visualization import plot_drr, plot_mask
 from timm.utils.agc import adaptive_clip_grad as adaptive_clip_grad_
 from tqdm import tqdm
 
-from ..config.trainer import args
 from .augmentations import XrayAugmentations
 from .loss import PoseRegressionLoss
 from .sampler import get_random_pose
@@ -19,54 +18,108 @@ from .utils import initialize_coordinate_frame, initialize_modules, initialize_s
 class Trainer:
     def __init__(
         self,
-        volpath,
-        maskpath,
-        outpath,
-        alphamin,
-        alphamax,
-        betamin,
-        betamax,
-        gammamin,
-        gammamax,
-        txmin,
-        txmax,
-        tymin,
-        tymax,
-        tzmin,
-        tzmax,
-        sdd,
-        height,
-        delx,
-        orientation=args.orientation,
-        reverse_x_axis=args.reverse_x_axis,
-        renderer=args.renderer,
-        parameterization=args.parameterization,
-        convention=args.convention,
-        model_name=args.model_name,
-        pretrained=args.pretrained,
-        norm_layer=args.norm_layer,
-        unit_conversion_factor=args.unit_conversion_factor,
-        p_augmentation=args.p_augmentation,
-        lr=args.lr,
-        weight_ncc=args.weight_ncc,
-        weight_geo=args.weight_geo,
-        weight_dice=args.weight_dice,
-        weight_mvc=args.weight_mvc,
-        batch_size=args.batch_size,
-        n_total_itrs=args.n_total_itrs,
-        n_warmup_itrs=args.n_warmup_itrs,
-        n_grad_accum_itrs=args.n_grad_accum_itrs,
-        n_save_every_itrs=args.n_save_every_itrs,
-        disable_scheduler=args.disable_scheduler,
-        ckptpath=None,
-        reuse_optimizer=args.reuse_optimizer,
-        warp=None,
-        invert=args.invert,
-        patch_size=None,
-        num_workers=args.num_workers,
-        pin_memory=args.pin_memory,
-        weights=None,
+        volpath: str,
+        maskpath: str,
+        outpath: str,
+        alphamin: float,
+        alphamax: float,
+        betamin: float,
+        betamax: float,
+        gammamin: float,
+        gammamax: float,
+        txmin: float,
+        txmax: float,
+        tymin: float,
+        tymax: float,
+        tzmin: float,
+        tzmax: float,
+        sdd: float,
+        height: int,
+        delx: float,
+        orientation: str = "AP",
+        reverse_x_axis: bool = False,
+        renderer: str = "trilinear",
+        parameterization: str = "quaternion_adjugate",
+        convention: str = "ZXY",
+        model_name: str = "resnet18",
+        pretrained: bool = True,
+        norm_layer: str = "groupnorm",
+        unit_conversion_factor: float = 1000.0,
+        p_augmentation: float = 0.5,
+        lr: float = 5e-3,
+        weight_ncc: float = 1e0,
+        weight_geo: float = 1e-2,
+        weight_dice: float = 1e0,
+        batch_size: int = 116,
+        n_total_itrs: int = 1_000_000,
+        n_warmup_itrs: int = 1_000,
+        n_grad_accum_itrs: int = 4,
+        n_save_every_itrs: int = 1_000,
+        disable_scheduler: bool = False,
+        ckptpath: str | None = None,
+        reuse_optimizer: bool = False,
+        warp: str | None = None,
+        invert: bool = False,
+        patch_size: tuple[int, int, int] | None = None,
+        num_workers: int = 4,
+        pin_memory: bool = False,
+        weights: list[float] | None = None,
+        img_threshold: float = 0.10,
+        mask_threshold: float = 0.05,
     ):
+        """Train a pose regression model.
+
+        Args:
+            volpath: CT or directory of CTs for pretraining.
+            maskpath: Optional labelmaps corresponding to the CTs.
+            outpath: Directory in which to save model weights.
+            alphamin: Minimum primary angle (in degrees).
+            alphamax: Maximum primary angle (in degrees).
+            betamin: Minimum secondary angle (in degrees).
+            betamax: Maximum secondary angle (in degrees).
+            gammamin: Minimum tertiary angle (in degrees).
+            gammamax: Maximum tertiary angle (in degrees).
+            txmin: Minimum x-offset (in millimeters).
+            txmax: Maximum x-offset (in millimeters).
+            tymin: Minimum y-offset (in millimeters).
+            tymax: Maximum y-offset (in millimeters).
+            tzmin: Minimum z-offset (in millimeters).
+            tzmax: Maximum z-offset (in millimeters).
+            sdd: Source-to-detector distance (in millimeters).
+            height: DRR height (in pixels).
+            delx: DRR pixel size (in millimeters / pixel).
+            orientation: Orientation of CT volumes.
+            reverse_x_axis: Horizontally flip the rendered DRRs.
+            renderer: DRR rendering backend ('trilinear' or 'siddon').
+            parameterization: Parameterization of SO(3) for regression.
+            convention: If parameterization='euler_angles', specify order.
+            model_name: Name of model to instantiate from the timm library.
+            pretrained: Load pretrained ImageNet-1k weights.
+            norm_layer: Normalization layer.
+            unit_conversion_factor: Scale factor for translation prediction (e.g., from m to mm).
+            p_augmentation: Base probability of image augmentations during training.
+            lr: Maximum learning rate.
+            weight_ncc: Weight on mNCC loss term.
+            weight_geo: Weight on geodesic loss term.
+            weight_dice: Weight on Dice loss term.
+            batch_size: Number of DRRs per batch.
+            n_total_itrs: Number of iterations for training the model.
+            n_warmup_itrs: Number of iterations for warming up the learning rate.
+            n_grad_accum_itrs: Number of iterations for gradient accumulation.
+            n_save_every_itrs: Number of iterations before saving a new model checkpoint.
+            disable_scheduler: Turn off cosine learning rate scheduler.
+            ckptpath: Checkpoint of a pretrained pose regressor.
+            reuse_optimizer: Initialize the previous optimizer's state.
+            warp: SimpleITK transform to warp input CT to checkpoint's reference frame.
+            invert: Whether to invert the warp or not.
+            patch_size: Optional random crop size; if None, return entire volume.
+            num_workers: Number of subprocesses to use in the dataloader.
+            pin_memory: Copy volumes into CUDA pinned memory before returning.
+            weights: Probability for sampling each volume in volpath.
+            img_threshold: Minimum fraction of foreground pixels to keep a DRR.
+            mask_threshold: Minimum fraction of mask pixels to keep a DRR.
+        """
+
         # Record all hyperparameters to be checkpointed
         self.config = locals()
         del self.config["self"]
@@ -116,9 +169,7 @@ class Trainer:
         )
 
         # Initialize the loss function
-        self.lossfn = PoseRegressionLoss(
-            sdd, weight_ncc, weight_geo, weight_dice, weight_mvc
-        )
+        self.lossfn = PoseRegressionLoss(sdd, weight_ncc, weight_geo, weight_dice)
 
         # Set up augmentations
         self.contrast_distribution = torch.distributions.Uniform(1.0, 10.0)
@@ -148,6 +199,8 @@ class Trainer:
         self.n_total_itrs = n_total_itrs
         self.n_grad_accum_itrs = n_grad_accum_itrs
         self.n_save_every_itrs = n_save_every_itrs
+        self.img_threshold = img_threshold
+        self.mask_threshold = mask_threshold
         self.outpath = outpath
 
     def train(self, run=None):
@@ -187,9 +240,7 @@ class Trainer:
         pose = get_random_pose(**self.pose_distribution).cuda()
 
         # Load the subject and translate the pose to its isocenter
-        vol, seg, affinv, offset = self.load(
-            subject, pose.matrix.dtype, pose.matrix.device
-        )
+        vol, seg, affinv, offset = self.load(subject, pose.matrix.dtype, pose.matrix.device)
         pose = pose.compose(offset)
 
         # Render a batch of DRRs and keep samples that capture the volume
@@ -214,16 +265,14 @@ class Trainer:
 
         # Compute the loss
         img, pred_img = self.transforms(img), self.transforms(pred_img)
-        loss, mncc, dgeo, rgeo, tgeo, dice, mvc = self.lossfn(
+        loss, mncc, dgeo, rgeo, tgeo, dice = self.lossfn(
             img, mask, pose, pred_img, pred_mask, pred_pose
         )
         loss = loss / self.n_grad_accum_itrs
 
         # Optimize the model
         loss.mean().backward()
-        if ((itr + 1) % self.n_grad_accum_itrs == 0) or (
-            (itr + 1) == self.n_total_itrs
-        ):
+        if ((itr + 1) % self.n_grad_accum_itrs == 0) or ((itr + 1) == self.n_total_itrs):
             adaptive_clip_grad_(self.model.parameters())
             self.optimizer.step()
             self.scheduler.step()
@@ -236,7 +285,6 @@ class Trainer:
             "rgeo": rgeo.mean().item(),
             "tgeo": tgeo.mean().item(),
             "dice": dice.mean().item(),
-            "mvc": mvc.mean().item(),
             "loss": loss.mean().item(),
             "lr": self.scheduler.get_last_lr()[0],
             "kept": keep.float().mean().item(),
@@ -276,9 +324,7 @@ class Trainer:
 
         return volume, mask, affinv, offset
 
-    def render_samples(
-        self, tmp, seg, affinv, pose, img_threshold=0.10, mask_threshold=0.05
-    ):
+    def render_samples(self, tmp, seg, affinv, pose):
         # Get the source and target locations for every ray in voxel coordinates
         source, target = self.drr.detector(pose, None)
         img = (target - source).norm(dim=-1).unsqueeze(1)
@@ -294,12 +340,10 @@ class Trainer:
 
         # Discard empty imgs/masks
         if mask.shape[1] == 1:
-            # Keep if >10% of the image is non-zero pixels
-            keep = mask.to(img).flatten(1).mean(1) > img_threshold
+            keep = mask.to(img).flatten(1).mean(1) > self.img_threshold
         else:
-            # Keep if >5% of the image contains pixels corresponding to masked structures
             keep = mask[:, 1:].sum(dim=1, keepdim=True)
-            keep = (keep > 0).to(img).flatten(1).mean(1) > mask_threshold
+            keep = (keep > 0).to(img).flatten(1).mean(1) > self.mask_threshold
 
         return img, mask, keep
 
