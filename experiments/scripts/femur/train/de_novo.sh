885d5608d9ded193a4d6dcf56c512d44fcda0e5e
eigenvivek <vivekg@mit.edu>
2026-07-17 07:09:54 -0400

    Pin GPU to A6000s

--- experiments/scripts/femur/train/de_novo.sh
+++ experiments/scripts/femur/train/de_novo.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=polina-all
 #SBATCH --qos=vision-polina-main
 #SBATCH --account=vision-polina
-#SBATCH --gres=gpu:rtx_6000_ada:1
+#SBATCH --gres=gpu:a6000:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=50G
 #SBATCH --time=24:00:00
