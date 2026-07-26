904c0ed5c8222ed01371c9df4e5490f3942731e6
eigenvivek <vivekg@mit.edu>
2026-07-26 05:56:38 -0400

    Return treshold mode to rel

--- src/xvr/register/register.py
+++ src/xvr/register/register.py
@@ -243,7 +243,7 @@ class Register:
             maximize=True,
         )
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer, "max", self.factor, patience, self.threshold, threshold_mode="abs"
+            optimizer, "max", self.factor, patience, self.threshold, threshold_mode="rel"
         )
         transform = XrayTransforms(drr.detector.height, drr.detector.width, equalize=equalize)
         return optimizer, scheduler, transform
