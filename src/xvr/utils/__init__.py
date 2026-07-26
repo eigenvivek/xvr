4209231fa1f2f311b9205cd18bf4a086d432649d
eigenvivek <vivekg@mit.edu>
2026-07-26 06:07:11 -0400

    Remove TF32 guard

--- src/xvr/utils/__init__.py
+++ src/xvr/utils/__init__.py
@@ -1,12 +1,4 @@
-import torch
-
 from .preprocess import Standardize, XrayTransforms
 from .transforms import read_rigid_transform
-
-# TF32 makes pose predictions differ across GPU architectures
-if hasattr(torch.backends.cudnn, "conv"):
-    torch.backends.cudnn.conv.fp32_precision = "ieee"
-else:
-    torch.backends.cudnn.allow_tf32 = False
 
 __all__ = ["Standardize", "XrayTransforms", "read_rigid_transform"]
